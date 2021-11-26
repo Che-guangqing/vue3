eb95280948bf876f83befeb79e14ec082e662bc5
车光青 <g.che@herin.ai>
2021-11-26 14:33:04 +0800

    ，

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 createApp(App).mount('#app')
 
+console.log(createApp(App).config);
+
 // const vm= new Vue({
 //   data() {},
 //   methods: {},
